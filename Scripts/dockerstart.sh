be6aba9763c75a85009e0c68993addaf5b7c6cff
zoglam <anzoor1998@gmail.com>
2021-02-24 00:41:58 +0300

    docker script deleted

--- Scripts/dockerstart.sh
+++
@@ -1,7 +0,0 @@
-until mysql -h $MARIA_HOST -p$MARIA_PASSWORD; do
-    >&2 echo "mariadb is unavailable - sleeping"
-    sleep 5
-done
->&2 echo "mariadb is up - executing commands"
-
-go run app/main.go
